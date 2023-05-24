aa029a7358aaef239e62aa1aaf982b126135f7bf
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-05-24 19:07:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,14 +14,24 @@ function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
 
-	for (let i = 0; i < encodedStr.length; i++) {
-    let char = encodedStr[i];
-    let decodedChar = lookup[char] || char;
-    decodedArr.push(decodedChar);
-  }
+	// for (let i = 0; i < encodedStr.length; i++) {
+ //    let char = encodedStr[i];
+ //    let decodedChar = lookup[char] || char;
+ //    decodedArr.push(decodedChar);
+ //  }
 
-  let decodedStr = decodedArr.join('');
-  return decodedStr;
+ //  let decodedStr = decodedArr.join('');
+ //  return decodedStr;
+
+	for (let i = 0; i < encodedStr.length; i++) {
+		if (lookup[ encodedStr[i] === undefined]) {
+			decodedArr += encodedStr[i] ;
+		} else {
+			decodedArr += lookup[ encodedStr[i] ];
+		}
+	}
+  return decodedArr;//return decodedArr
+
 }
 
 // Example usage
